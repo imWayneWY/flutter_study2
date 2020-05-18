e0bac85657912df5ea73f0695bf6aad70b549c29
weiyan <im.wayne.wy@gmail.com>
2020-05-18 16:02:37 -0600

    finish course 8

--- Clima-Flutter/lib/services/weather.dart
+++ Clima-Flutter/lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = 'e72ca729af228beabd5d20e3b7749713';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async{
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async{
     Location location = Location();
     await location.getCurrentLocation();
@@ -14,7 +21,7 @@ class WeatherModel {
     var weatherData = await networkHelper.getData();
     return weatherData;
   }
-  
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
